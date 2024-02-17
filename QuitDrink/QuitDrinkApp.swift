4eeb90e95e4e182cfed2999a2201bdfe782227de
Georgii Fesenko <fesenko.g@gmail.com>
2024-02-17 17:26:59 +0300

    Initial Commit

---
+++ QuitDrink/QuitDrinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuitDrinkApp.swift
+//  QuitDrink
+//
+//  Created by Georgii Fesenko on 17.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct QuitDrinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
